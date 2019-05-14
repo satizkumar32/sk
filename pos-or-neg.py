ad014b49a01c63aba2da8d362427c737dc174b52
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-05-14 14:02:54 +0530

    Add files via upload

---
+++ pos-or-neg.py
@@ -0,0 +1,5 @@
+number = input ("Enter number")
+number_two = input("Enter another number")
+print("Printing type of input value")
+print ("type of number ", type(number))
+print ("type of number_two ", type(number_two ))
