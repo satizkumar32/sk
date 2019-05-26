b0a56ab4b243e0c5190eef9eb2477b7d90e8d8f9
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-05-25 20:50:44 -0700

    Update Largest.py

--- Largest.py
+++ Largest.py
@@ -1,9 +1,19 @@
-a=int(input(""))
-b=int(input(""))
-c=int(input(""))
-if(a>b and a>c):
-	print(a)
-elif(b>c):
-	print(b)
-else:
-	print(c)
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))	
+	{
+	printf("%d",a);
+	}
+else if(b>c)
+	{
+	printf("%d",b);
+	}
+else
+	{
+		printf("%d",c);
+	}
+	
+}
