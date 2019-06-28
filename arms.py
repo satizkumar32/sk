8e65e71cc75bad9d62cedb7a1aa831feb61e8afb
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-28 11:20:02 +0530

    Create arms.py

---
+++ arms.py
@@ -0,0 +1,10 @@
+r1,r2=map(int,input().split())
+for n in range(r1+1,r2):
+    if(0<n<10):
+        print(n,end=" ")
+    elif(n==153 or n==370 or n==371 or n==407):
+        print(n,end=" ")
+    elif(n==1634 or n==8203 or n==9474):
+        print(n,end=" ")
+    elif(n==54748 or n==92727 or n==93084):
+        print(n,end=" ")
