ed9970bc087d1b81858b3ffd3a163a366d240580
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-28 17:24:25 +0530

    Update hun9.py

--- hun9.py
+++ hun9.py
@@ -4,7 +4,7 @@ os=max(hs)
 xx,yy=0,0
 for i in range(0,len(hs)-1):
   for j in range(i+1,len(hs)):
-    if ab(hs[i]+hs[j])<os:
+    if abs(hs[i]+hs[j])<os:
       xx,yy=hs[i],hs[j]
-      os=ab(xx+yy)
+      os=abs(xx+yy)
 print(x,y)
