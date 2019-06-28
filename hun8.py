78293bd4fcad8a864f29aadbd9f89c555193d116
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-28 17:16:33 +0530

    Update hun8.py

--- hun8.py
+++ hun8.py
@@ -4,4 +4,4 @@ for aa in range(0,ee-2):
     for bb in range(aa+1,ee-1):
         for cc in range(bb+1,ee):
             if(dd[aa]+dd[bb]==dd[cc]):
-                print(dd[aa], dd[bb], dd[cc]
+                print(dd[aa], dd[bb], dd[cc])
