ac4f899895bf6d9a13ce7efcab44ccc5f0dfa977
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-28 12:46:27 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,5 +1,5 @@
 mash=input()
-l=[ash[i] for i in range(len(mash)) if i%2==0]
-l1=[ash[i] for i in range(len(mash)) if i%2!=0]
+l=[mash[i] for i in range(len(mash)) if i%2==0]
+l1=[mash[i] for i in range(len(mash)) if i%2!=0]
 for j in range(len(mash)//2):
   print(l1[j]+l[j],end="")
