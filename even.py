5c86bc5c3a85725524b86b4fc23c87166f7de731
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-28 10:59:28 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,8 @@
+n,q=map(int,input().split())
+for n in range(n,q + 1):
+    if n > 1:
+        for i in range(2,n):
+            if (n % i) == 0:
+                break
+        else:
+            print(n,end=" ")
