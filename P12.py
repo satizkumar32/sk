7e317f5ef505c57110984877cf968994865a2ba0
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-30 18:09:03 +0530

    Create P12.py

---
+++ P12.py
@@ -0,0 +1,13 @@
+aas,bb=map(int,input().split())
+cc=[]
+dd=[]
+ee=[int(aas) for aas in input().split()]
+for i in range(0,bb):
+    u,v=map(int,input().split())
+    for j in range(u-1 ,v):
+        dd.append(ee[j])
+    vj=sum(dd)
+    cc.append(vj)
+print(cc[0])
+for x in range(1,len(cc)):
+    print(cc[x]- cc[x- 1])
