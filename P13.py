19a20dd1ecbef6c92377d800e9989fec15a072f7
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-30 18:12:21 +0530

    Create P13.py

---
+++ P13.py
@@ -0,0 +1,14 @@
+
+vjs,vs=map(int,input().split())
+vk=[]
+ss=[]
+a=[int(vjs) for vjs in input().split() ]
+for i in range(0,vs):
+    u,v=map(int,input().split())
+    for j in range(u-1 ,v):
+        ss.append(a[j])
+    x=sorted(ss)
+    vk.append(min(ss))
+    del ss[:]
+for sz in range(0,len(vk)):
+    print(vk[sz])
