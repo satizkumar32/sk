6648f1853f3590ca7fddd81065c2aecc29bd342c
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-06-30 11:15:34 +0530

    Update P2.py

--- P2.py
+++ P2.py
@@ -2,7 +2,7 @@ from itertools import combinations
 ns,n2=input().split()
 n2=int(n2)
 l=[]
-dd=combinations(n,len(ns)-n2)
+dd=combinations(ns,len(ns)-n2)
 for i in list(dd):
   l.append("".join(i))
 print(min(l))
