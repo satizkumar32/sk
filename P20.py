5fc0c0210469230de945a4b2bbc25ea0b18800e0
satizkumar32 <50615791+satizkumar32@users.noreply.github.com>
2019-07-01 18:52:07 +0530

    Update P20.py

--- P20.py
+++ P20.py
@@ -1,17 +1,17 @@
-as,by=map(int,input().split())
-h=list(map(int,input().split()))
-h.sort()
-h.reverse()
-as=by
-z=0
-for i in h:
-    if(as>=i):
-        nm=int(as/i)
-        z=z+n
-        as=as-nm*i
-    if as==0:
+ns,k=map(int,input().split())
+ms=list(map(int,input().split()))
+ms.sort()
+ms.reverse()
+sum=k
+p=0
+for i in ms:
+    if(sum>=i):
+        a=int(sum/i)
+        p=p+a
+        sum=sum-a*i
+    if sum==0:
         break
-if(as==0):
-   print(z)
+if(sum==0):
+   print(p)
 else:
-     print("it's not posiible to select coins in such a way that they sum upto",S)  
+   print("it's not posiible to select coins in such a way that they sum upto",k)
